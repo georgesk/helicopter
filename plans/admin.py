8cf039027d93a1a1fbb5e0f17dd3cf3cc8529be8
Georges Khaznadar <georgesk@debian.org>
2016-10-31 11:55:22 +0100

    un paramètre pour le plan

--- plans/admin.py
+++ plans/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from plans.models import Plan
 
 # Register your models here.
+
+class PlanAdmin (admin.ModelAdmin):
+    class Media:
+        js = (
+            '/static/plans/js/jquery/jquery.js',
+            '/static/plans/js/jquery-ui/jquery-ui.js',
+)
+        css = {
+            'all': (
+                '/static/plans/css/base.css',
+                '/static/plans/js/jquery-ui/css/smoothness/jquery-ui.min.css',
+            ),
+        }
+    fields = ('hauteur_helice',)
+
+admin.site.register(Plan, PlanAdmin)
--- plans/models.py
+++ plans/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.forms import Form, ModelForm, ValidationError, Select
 
 # Create your models here.
+
+hauteur_helice_CHOICES = [(n,n) for n in range(65,126)]
+class Plan(models.Model):
+    hauteur_helice = models.IntegerField(
+        verbose_name = "Hauteur de l'hélice (mm)",
+        default      = 70,
+        choices      = hauteur_helice_CHOICES,
+    )
+
